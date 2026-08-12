54e4642c788505fc41ce431b70cff0ad50451209
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-08-12 21:45:30 +0530

    Final commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:import.meta.env.MODE_ENV === "production" ? "/api" : "http://localhost:3000/api",
     withCredentials:true
 })
